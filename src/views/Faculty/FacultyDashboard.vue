78f16a5a8d7e080dd81510b56bbd43e02f44d434
akhilkasuvojula <77549243+akhilkasuvojula@users.noreply.github.com>
2022-02-20 12:35:22 -0600

    created FacultyDashboard.vue and made commit

---
+++ src/views/Faculty/FacultyDashboard.vue
@@ -0,0 +1,85 @@
+<template >
+  <div class="overflow-x: hidden">  
+<div class="bg-white">
+<b-row class="align-inside">
+  <FacultyView class="content-style"/>
+          
+</b-row>
+ </div>
+</div>
+</template>
+
+<script>
+ import FacultyView from "./FacultyView.vue";
+
+  export default {
+     components: {
+           FacultyView,
+       },
+   
+    methods: {
+        refreshTable: function(){
+            this.$refs.exampleTable.refresh();
+        },
+        setNewPageSize:function(){
+            this.$refs.exampleTable.setPageSize(1);
+        },
+        addItem: function () {
+            const item = {
+                "id": this.values.length + 1,
+                "name": "name " + (this.values.length + 1)
+            };
+            this.values.push(item);
+        },
+        toggleFilter: function () {
+            this.showFilter = !this.showFilter;
+        },
+        togglePicker: function () {
+            this.showPicker = !this.showPicker;
+        },
+        toggleSelect: function () {
+            this.showSelect = !this.showSelect;
+        },
+        togglePagination: function () {
+            this.paginated = !this.paginated;
+        }
+    },
+    data() {
+      return {
+        paginate: {
+        limit: 10,
+        skip: 1,
+      },
+        perPage: 5,
+        currentPage: 1,
+        items: [
+         
+        ]
+      }
+    },
+    computed: {
+      rows() {
+        return this.items.length
+      }
+    }
+  }
+</script>
+<style scoped>
+.bg-white{
+  background-color: white;
+}
+.overflow-auto{
+    padding :15px;
+    margin:0 5px 0 15px;
+}
+.align-inside{
+  padding: 0 0% 0 0%;
+  justify-content:center;
+  width: 100%;
+}
+.content-style{
+  padding: 0 0% 0 0%;
+  width: 100%;
+}
+
+</style>
