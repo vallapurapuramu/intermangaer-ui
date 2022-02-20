942fb7e0778807bcf3dfb4e6307add1424173eae
s541910 <s541910@nwmissouri.edu>
2022-02-20 12:46:34 -0600

    added footer vue

---
+++ src/components/Semantic/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="footer">Learning & Teaching Center | Northwest Missouri State University</div>
+</template>
+
+<style scoped>
+.footer {
+  background-color: #006747;
+  text-align: right;
+  color: whitesmoke;
+  padding: 10px;
+}
+</style>
