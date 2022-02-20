4b6ad06760ba3cb0f3374a135249e21b0c20c5d1
VENKATESH VEMULA <77640153+Vemula23@users.noreply.github.com>
2022-02-20 12:55:13 -0600

    Added components/Map.vue

---
+++ src/components/Map.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="map-container">
+    <l-map :center="[42.032974, -99.082029]" :zoom="4" style="height: 500px;" :options="mapOptions">
+      <l-choropleth-layer :data="pyDepartmentsData" titleKey="department_name" idKey="department_id" :value="value" :extraValues="extraValues" geojsonIdKey="dpto" :geojson="paraguayGeojson" :colorScale="colorScale">
+      </l-choropleth-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+//import { InfoControl, ReferenceChart, ChoroplethLayer } from 'vue-choropleth'
+import paraguayGeojson from '../data/paraguay.json'
+import { pyDepartmentsData } from '../data/py-departments-data'
+import {LMap} from 'vue2-leaflet';
+
+export default {
+ 
+  components: { 
+    LMap,
+       //'l-info-control': InfoControl, 
+      //'l-reference-chart': ReferenceChart, 
+      'l-choropleth-layer': ChoroplethLayer 
+  },
+  data() {
+    return {
+      pyDepartmentsData,
+      paraguayGeojson,
+      colorScale: ["e7d090", "e9ae7b", "de7062"],
+      value: {
+        key: "amount_w",
+        metric: "% girls"
+      },
+      extraValues: [{
+        key: "amount_m",
+        metric: "% boys"
+      }],
+      mapOptions: {
+        attributionControl: false
+      },
+      currentStrokeColor: '3d3213',
+    }
+  },
+}
+</script>
+<style>
+
+</style>
