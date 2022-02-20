689f2aa9ca6c9b0e0d4bdb57ffbf16c443a32076
anusha kanagala <67909209+Anushakanagala@users.noreply.github.com>
2022-02-20 12:51:51 -0600

    added 404 vue.vue

---
+++ src/components/Error404Page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Error 404 Page</h1>
+    <router-link :to="{name: 'dashboard'}">click here</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error404Page"
+};
+</script>
+
+<style scoped>
+</style>
